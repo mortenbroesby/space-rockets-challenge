061f82979c245d699fe6b33a20f73e6fec05daab
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-03-22 22:22:16 +0100

    refactor: Migrate logic to Application

--- src/application/app.tsx
+++ src/application/app.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { Routes, Route } from "react-router-dom";
 import { Flex, Text } from "@chakra-ui/core";
 
@@ -8,18 +10,34 @@ import Home from "../components/home";
 import LaunchPads from "../components/launch-pads";
 import LaunchPad from "../components/launch-pad";
 
-export default function App() {
+export function Application() {
+  return (
+    <Router>
+      <ThemeProvider>
+        <CSSReset />
+        <RootComponents />
+      </ThemeProvider>
+    </Router>
+  );
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/launches" element={<Launches />} />
+      <Route path="/launches/:launchId" element={<Launch />} />
+      <Route path="/launch-pads" element={<LaunchPads />} />
+      <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
+    </Routes>
+  );
+}
+
+function RootComponents() {
   return (
     <div>
       <NavBar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/launches" element={<Launches />} />
-        <Route path="/launches/:launchId" element={<Launch />} />
-        <Route path="/launch-pads" element={<LaunchPads />} />
-        <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
-      </Routes>
+      <AppRoutes />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
-import App from "./application/app";
+import { Application } from "./application/app";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider>
-        <CSSReset />
-        <App />
-      </ThemeProvider>
-    </Router>
+    <Application />
   </React.StrictMode>,
   document.getElementById("root")
 );
