aa17c289481b789af116db2eff8e8a07192d63a0
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-03-22 22:22:50 +0100

    refactor: Rename application files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Application } from "./application/app";
+import { Application } from "./application";
 
 ReactDOM.render(
   <React.StrictMode>
