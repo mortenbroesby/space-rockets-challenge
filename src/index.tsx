b016dcd9db18e74fdd44738ee4941e72894e5756
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-03-23 10:36:31 +0100

    fix: Remove service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import { Application } from "./application";
 
 import { reportWebVitals } from "./infrastructure";
-import * as serviceWorker from "./service-worker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,9 +11,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// See: https://cra.link/PWA
-serviceWorker.register();
 
 // See: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
