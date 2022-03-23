b24538c7910ee82068abf052556f356c34743801
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-03-23 10:16:03 +0100

    feat: Add service-worker and perf-monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom";
 
 import { Application } from "./application";
 
+import { serviceWorker, reportWebVitals } from "./infrastructure";
+
 ReactDOM.render(
   <React.StrictMode>
     <Application />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// See: https://cra.link/PWA
+serviceWorker.register();
+
+// See: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
