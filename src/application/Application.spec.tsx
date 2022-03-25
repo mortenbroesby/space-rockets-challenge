3e9cff4449ae7e8f9d03463ca96f825695f28486
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-03-25 13:44:46 +0100

    chore: Remove placeholder test

--- src/application/Application.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Application } from "./Application";
-
-test("Placeholder test passes", () => {
-  render(<Application />);
-  expect(true).toBe(true);
-});
